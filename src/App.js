e160d4f1a916ce4be4582a62d54b7af27cac659c
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2019-10-30 07:14:20 +0200

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+import Dashboard from "./component/Dashboard";
+
+import "./styles/base.css";
+import "./styles/layout.css";
+import "./styles/modules/common.css";
+import "./styles/modules/nav.css";
+import "./styles/modules/topbar.css";
+import "./styles/modules/files.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <section className="App">
+        <Route exact path="/" component={Dashboard} />
+      </section>
+    </BrowserRouter>
   );
 }
 
